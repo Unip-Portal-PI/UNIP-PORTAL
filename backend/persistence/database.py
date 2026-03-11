838b79d5247b6c7c9989c6d5baf35666d07e665f
essyefe <samufdev@gmail.com>
2026-03-11 15:59:31 -0300

    chore: Configurando engine do SQLAlchemy e dependência de sessão get_db

---
+++ backend/persistence/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from core.config import settings 
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True,           
+    pool_size=20,         
+    max_overflow=30,      
+    pool_timeout=60,      
+    pool_recycle=1800     
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
