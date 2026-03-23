a62a4b84dfb9a5655d87c899fe9fe61d7fc34489
Ademar Rafhael <vnarafha@gmail.com>
2026-03-22 22:25:35 -0300

    chore(db): change qr_code_usuario to text

--- backend/app/models/inscricao.py
+++ backend/app/models/inscricao.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey, UniqueConstraint, text
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey, UniqueConstraint, text
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
 from app.core.database import Base
@@ -13,7 +13,7 @@ class InscricaoModel(Base):
     id_inscricao = Column(String(36), primary_key=True, server_default=text("(UUID())"))
     id_evento = Column(String(36), ForeignKey("evento.id_evento", ondelete="CASCADE"), nullable=False)
     id_usuario = Column(String(36), ForeignKey("usuario.id_usuario"), nullable=False)
-    qr_code_usuario = Column(String(255), nullable=True, unique=True)
+    qr_code_usuario = Column(Text, nullable=True)
     data_inscricao = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
 
     evento = relationship("EventoModel", back_populates="inscricoes")
